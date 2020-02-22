f8b454fd4658b32cae71a9380bc240754df7dacb
tcatapano <thc4ster@gmail.com>
2020-02-22 16:35:21 +0100

    re-run link check

---
+++ check_hrefs.sh
@@ -0,0 +1,3 @@
+xmlstarlet sel -t -v "//a/@href[starts-with(.,http)]" -n *.html | sort | uniq -c | sort -nr > ../logs/href_frq.txt
+xmlstarlet sel -t -v "//a/@href[starts-with(.,http)]" -n *.html | sort -u > ../logs/href_uniq.txt
+wget -nv --spider -t 2 -i ../logs/href_uniq.txt -o ../logs/href_spider_log.txt
