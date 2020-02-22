c0dfa307fe0ccd3d86dfab68adbcc981c9fc340d
tcatapano <thc4ster@gmail.com>
2020-02-22 17:58:55 +0100

    Update check_hrefs.sh

--- check_hrefs.sh
+++ check_hrefs.sh
@@ -1,3 +1,3 @@
-xmlstarlet sel -t -v "//a/@href[starts-with(.,http)]" -n *.html | sort | uniq -c | sort -nr > ../logs/href_frq.txt
-xmlstarlet sel -t -v "//a/@href[starts-with(.,http)]" -n *.html | sort -u > ../logs/href_uniq.txt
-wget -nv --spider -t 2 -i ../logs/href_uniq.txt -o ../logs/href_spider_log.txt
+xmlstarlet sel -t -v "//a/@href[starts-with(.,http)]" -n *.html | sort | uniq -c | sort -nr > logs/href_frq.txt
+xmlstarlet sel -t -v "//a/@href[starts-with(.,http)]" -n *.html | sort -u > logs/href_uniq.txt
+wget -nv --spider -t 1 -i logs/href_uniq.txt -o logs/href_spider_log.txt
